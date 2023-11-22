23cedb833341e5306b403391eb409f82200cd391
frank basham <frankabasham@gmail.com>
2023-11-22 09:04:33 -0500

    add middleware, i18n, more structure

---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+  en: "English",
+  fr: "Français",
+};
+
+export type Ui = {
+  [key: string]: { [key: string]: string };
+};
+
+export const ui: Ui = {
+  en: {
+    "home": "Home",
+  },
+  fr: {
+    "home": "Accueil",
+  },
+};
