05aab088a4df08ea4d2a990010c58622c78cb7c8
frank basham <frankabasham@gmail.com>
2023-11-22 11:48:08 -0500

    scaffold out layout

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,9 @@ export function onRequest({ request, redirect }, next) {
   const url = new URL(request.url);
   const pathname = url.pathname;
 
-  if (pathname === "/" || /\/(en|fr)\//.test(pathname)) {
+  if (pathname === "/" || /^\/(en|fr)\//.test(pathname)) {
     return next();
   }
+
   return Response.redirect(new URL("/404", request.url), 302);
 }
